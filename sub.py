8936947bbfa63ee8df48821b643e8684dc2394ca
VSRohith <153886638+VSRohith@users.noreply.github.com>
2024-05-02 09:37:58 +0530

    new sub.py

--- sub.py
+++ sub.py
@@ -3,3 +3,11 @@ num2 = 12
 sum = num1 + num2
 print("Sum",sum)
 print("Sum of", num1, "and", num2 , "is", sum)
+git cd suraj
+git status
+git add .py
+git status
+git commit -m ""
+git branch
+git push origin main
+git pull
